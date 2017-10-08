024195e6276f834ee6153b375aa24e9714c1c363
Lindsay Criswell <lindsaycriswell@gmail.com>
2017-10-08 19:33:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,11 +35,10 @@ function destructivelyRemoveElementFromEndOfArray(array){
   array.pop();
   return array;
 }
-// function destructivelyRemoveElementFromEndOfArray(array){
-//   array.pop();
-//   return array;
-// }
-//
+
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length-1);
+}
 // function removeElementFromEndOfArray(array){
 //   newArray = array.slice(0, array.length-1);
 //   return newArray;
